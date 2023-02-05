eb010748c3735294448e604d2e684eede74c9508
Parham <parham.parnian@gmail.com>
2023-02-05 15:08:31 +0330

    ping route to retrive requests

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['BotInputValidate'])->post("ping", function (Request $request) {
+    \Log::info($request->all());
+});
